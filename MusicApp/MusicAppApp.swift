bb0bba32b0cf24d2bdf3126aa18cb2edf4eacddf
SenqaIT <teddyyout21@gmail.com>
2024-07-31 13:56:13 +0300

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by arsen on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
