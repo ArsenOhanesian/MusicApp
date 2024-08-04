8cc103978b799fd40ab0ae8fbf4947c0ddfbdeb6
ArsenOhanesian <teddyyout21@gmail.com>
2024-08-04 12:44:29 +0300

    Main: Added font extension for duraton time

--- MusicApp/Helpers/Extensions/Font + extension.swift
+++ MusicApp/Helpers/Extensions/Font + extension.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct DurationFontModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(.white)
+            .font(.system(size: 14, weight: .light, design: .rounded))
+    }
+}
+
+extension View{
+    func durationFont() -> some View{
+        self.modifier(DurationFontModifier())
+    }
+}
+
 extension Text{
     func nameFont() -> some View{
         self
@@ -30,7 +44,18 @@ struct FontView: View {
                 .nameFont()
             Text("Artist Font")
                 .artistFont()
+            
+            HStack{
+                Text("00:00")
+                Spacer()
+                Text("03:27")
+                    
+            }
+            .durationFont()
+            .padding()
         }
+        
+       
     }
 }
 
