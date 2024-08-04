54967a89cc8946d471ff05f4d05a2ef4b34c4dac
ArsenOhanesian <teddyyout21@gmail.com>
2024-08-04 15:53:35 +0300

    Main: updated model for realm storage

--- MusicApp/Model/Model.swift
+++ MusicApp/Model/Model.swift
@@ -6,12 +6,32 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct SongModel: Identifiable{
-    var id = UUID()
-    var name: String
-    var data: Data
-    var author: String?
-    var coverImage: Data?
-    var duration: TimeInterval?
+//struct SongModel: Identifiable{
+//    var id = UUID()
+//    var name: String
+//    var data: Data
+//    var author: String?
+//    var coverImage: Data?
+//    var duration: TimeInterval?
+//}
+
+class SongModel: Object, ObjectKeyIdentifiable{
+   
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var name: String
+    @Persisted var data: Data
+    @Persisted var author: String?
+    @Persisted var coverImage: Data?
+    @Persisted var duration: TimeInterval?
+    
+    convenience init(name: String, data: Data, author: String? = nil, coverImage: Data? = nil, duration: TimeInterval? = nil) {
+        self.init()
+        self.name = name
+        self.data = data
+        self.author = author
+        self.coverImage = coverImage
+        self.duration = duration
+    }
 }
