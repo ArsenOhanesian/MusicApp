389a04f9fdc1a58dc98ce8fddff8a73864f910aa
ArsenOhanesian <teddyyout21@gmail.com>
2024-08-04 11:35:24 +0300

    Main: Created time Formatted

--- MusicApp/ViewModel/ViewModel.swift
+++ MusicApp/ViewModel/ViewModel.swift
@@ -11,4 +11,13 @@ class ViewModel: ObservableObject{
     
     @Published var songs: [SongModel] = []
     
+    
+    func durationFormatted(_ duration: TimeInterval) -> String{
+        let timeFormatter = DateComponentsFormatter()
+        timeFormatter.allowedUnits = [.minute, .second]
+        timeFormatter.unitsStyle = .positional
+        timeFormatter.zeroFormattingBehavior = .pad
+        
+        return timeFormatter.string(from: duration) ?? ""
+    }
 }
